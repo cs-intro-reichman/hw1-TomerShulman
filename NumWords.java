186076f81dfbb5a49429d345cf59e226f7a74ebb
TomerShulman <tomer.shulman@post.runi.ac.il>
2024-11-14 10:02:37 +0200

    test

--- NumWords.java
+++ NumWords.java
@@ -2,6 +2,6 @@
 public class NumWords {
 	public static void main(String args[]) {
 	    int Num = Integer.parseInt(args[0]);
-		System.out.println((Num/100) +" hundreds"+", "+(Num/10%10) +" tens ,and "+ (Num%10) +" ones");
+		System.out.println((Num/100) +" hundreds, "+(Num/10%10) +" tens, and "+ (Num%10) +" ones.");
 	}
 }
